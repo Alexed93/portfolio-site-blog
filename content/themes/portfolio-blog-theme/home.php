e194516d9567941e7ec4ad6d779aab40b1fc0f43
Alex Edwards <alexed93@gmail.com>
2016-11-12 14:10:23 +0000

    Fixed grid spacing bug

--- content/themes/portfolio-blog-theme/home.php
+++ content/themes/portfolio-blog-theme/home.php
@@ -21,13 +21,6 @@ get_header();
 <main class="section">
     <div class="container | cf">
     <?php get_sidebar('front'); ?>
-    <?php if (is_category('web')) : ?>
-    <?php get_sidebar('web'); ?>
-    <?php elseif (is_category('graphics')) : ?>
-    <?php get_sidebar('graphics'); ?>
-    <?php elseif (is_category('blogs')) : ?>
-    <?php get_sidebar('blogs'); ?>
-    <?php endif; ?> 
 
     <?php get_template_part('tpl-sideproject_loop'); ?>
 
