ab6bddc4505128044d43507b55a3a0802baeb090
Alex Edwards <alexed93@gmail.com>
2016-11-13 15:37:10 +0000

    Added smooth scrolling

--- content/themes/portfolio-blog-theme/assets/app/js/main.js
+++ content/themes/portfolio-blog-theme/assets/app/js/main.js
@@ -285,6 +285,22 @@ $(".dropdown_list").on("click",function(){
     $(".dropdown_list").toggleClass("dropdown_list--active");
 });
 
+/* $. Smooth scrolling
+\*----------------------------------------------------------------*/
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
 })(jQuery);
 
 /* $. Grunticon Load
