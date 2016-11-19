7e3cf84642e37db2d2d1386459d76d5447c2484f
Alex Edwards <alexed93@gmail.com>
2016-11-19 21:52:21 +0000

    Checkpoint

--- content/themes/portfolio-blog-theme/views/page/homepage/homepage-hero.php
+++ content/themes/portfolio-blog-theme/views/page/homepage/homepage-hero.php
@@ -1,10 +1,11 @@
-<div class="container_hero | container | cf">
+<div class="container_hero | container">
     <div class="splash_logo">
         <a href="/#projects" class="logo | logo--portfolio">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/purpsmall-01.svg">
         </a>
     </div>
-    <span id="projects"></span><div class="splash-text">
+    <span id="projects"></span>
+    <div class="splash-text">
         <h1>I'm Alex Edwards.</h1>
         <h2 class="highlight">I do web things.</h2>
         <h3 class="u-push-top/2">I study Creative Media Technology at Leeds Beckett University and i'm currently on placement as a Junior Front-end developer.</h3>
