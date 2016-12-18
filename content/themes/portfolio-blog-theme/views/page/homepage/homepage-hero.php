9a8b986c155ec8b119cd0c4d38486f3d98033bcf
Alex Edwards <alexed93@gmail.com>
2016-12-18 18:45:14 +0000

    Midway through making home splash better responsive

--- content/themes/portfolio-blog-theme/views/page/homepage/homepage-hero.php
+++ content/themes/portfolio-blog-theme/views/page/homepage/homepage-hero.php
@@ -1,14 +1,16 @@
 <div class="container_hero | container">
-    <div class="splash_logo">
-        <a href="#enter">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/purpsmall-01.svg" class="logo--portfolio">
-        </a>
-    </div>
-    <div id="enter"></div>
-    <div class="splash-text">
-        <h1>I'm Alex Edwards.</h1>
-        <h2 class="highlight">I do web things.</h2>
-        <h3 class="u-push-top/2">I study Creative Media Technology at Leeds Beckett University and i'm currently on placement as a Junior Front-end developer.</h3>
+    <div class="hero">
+        <div class="splash_logo">
+            <a href="#enter">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/purpsmall-01.svg" class="logo--portfolio">
+            </a>
+        </div>
+        <div id="enter"></div>
+        <div class="splash-text">
+            <h1>I'm Alex Edwards.</h1>
+            <h2 class="highlight">I do web things.</h2>
+            <h3 class="u-push-top/2">I study Creative Media Technology at Leeds Beckett University and i'm currently on placement as a Junior Front-end developer.</h3>
+        </div>
     </div>
 </div>
 
