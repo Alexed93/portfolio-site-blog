096763d1af4444971a98d6ff5ada4febecf5a162
Alex Edwards <alexed93@gmail.com>
2016-12-23 23:52:05 +0000

    Wordpressify stats and testimonials

--- content/themes/portfolio-blog-theme/views/page/homepage/homepage-stats.php
+++ content/themes/portfolio-blog-theme/views/page/homepage/homepage-stats.php
@@ -1,12 +1,18 @@
+<?php 
+
+    $fields = get_fields(222);
+
+?>
+
 <div class="section | section_stats">
     <div class="container">
         <div class="secondary-introduction | cf">
             <div class="secondary_heading">
-                <h1>What I can do.</h1>
-                <h2 class="highlight">The interesting bit.</h2>
+                <h1><?php echo $fields['skills_title'] ?></h1>
+                <h2 class="highlight"><?php echo $fields['skills_subtitle'] ?></h2>
             </div>
             <div class="secondary_excerpt">
-                <h3>Coding isn't my only skill. I'm also proficient with the Adobe Creative Suite, including Photoshop and Illustrator. Take a look at my top skills below.</h3>
+                <h3><?php echo $fields['skills_blurb'] ?></h3>
             </div>
         </div>
         <div class="statcircle | statcircle-1">
--- content/themes/portfolio-blog-theme/views/page/homepage/homepage-testimonials.php
+++ content/themes/portfolio-blog-theme/views/page/homepage/homepage-testimonials.php
@@ -1,26 +1,28 @@
+<?php 
+
+    $fields = get_fields(222);
+
+?>
 <div class="section | section_testimonials">
     <div class="container">
         <div class="secondary-introduction | cf">
             <div class="secondary_heading">
-                <h1>Testimonials.</h1>
-                <h2 class="highlight">What clients have said.</h2>
+                <h1><?php echo $fields['testimonials_title'] ?></h1>
+                <h2 class="highlight"><?php echo $fields['testimonials_subtitle'] ?></h2>
             </div>
             <div class="secondary_excerpt">
                 <div class="testimonial-slider">
                     <ul>
+                        <?php $testimonials = get_field('testimonial_content'); ?>
+                        <?php if($testimonials): ?>
+                        <?php foreach($testimonials as $testimonial) : ?>
                         <li>
-                            <p class="gamma">"Alex has been a pleasure to work with from start to finish, dragging us back into modern times."</p>
-                            <p class="gamma | u-float-right">- Chameleon</p>
+                            <p class="gamma">"<?php echo $testimonial['testimonial_text']; ?>"</p>
+                            <p class="gamma | u-float-right">- <?php echo $testimonial['testimonial_author']; ?></p>
                         </li>
-                        <li>
-                            <p class="gamma">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."</p>
-                            <p class="gamma | u-float-right">- Name</p>
-                        </li>
-                        <li>
-                            <p class="gamma">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."</p>
-                            <p class="gamma | u-float-right">- Name</p>
-                        </li>
+                        <?php endforeach; ?>
                     </ul>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
