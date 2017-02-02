a8b922679a42d4409fc6aca4cbbe5de393b3e834
Alex Edwards <alexed93@gmail.com>
2017-02-02 22:27:19 +0000

    Configure slack for deployment p2

--- config/slack.rb
+++ config/slack.rb
@@ -4,7 +4,7 @@
 
 # Required
 set :slack_subdomain, 'mixd' # if your Slack subdomain is example.slack.com
-set :slack_url, 'slack_token' # https://hooks.slack.com/services/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
+set :slack_url, 'https://hooks.slack.com/services/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' # https://hooks.slack.com/services/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
 
 # Optional
 set :slack_channel, '#alex'
