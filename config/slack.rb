e85c31b316f247c97c3fda108741c01dde607beb
Alex Edwards <alexed93@gmail.com>
2017-02-03 22:02:54 +0000

    Slick lightbox CSS

--- config/slack.rb
+++ config/slack.rb
@@ -7,5 +7,5 @@ set :slack_subdomain, 'mixd' # if your Slack subdomain is example.slack.com
 set :slack_url, 'https://hooks.slack.com/services/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' # https://hooks.slack.com/services/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
 
 # Optional
-set :slack_channel, '#alex'
+set :slack_channel, '@alex'
 set :slack_emoji, ':rocket:'
